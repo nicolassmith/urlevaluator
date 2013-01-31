5af67875157ea970ffe49da57f35e030d8e47c95
Nicolas Smith <nicolas.d.smith@gmail.com>
2013-01-31 17:13:42 -0600

    start setting up intent filter

--- src/com/github/nicolassmith/urlevaluator/UrlEvaluatorActivity.java
+++ src/com/github/nicolassmith/urlevaluator/UrlEvaluatorActivity.java
@@ -1,13 +1,20 @@
 package com.github.nicolassmith.urlevaluator;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class UrlEvaluatorActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //setContentView(R.layout.main);
+        
+        Uri inputUri = getIntent().getData();
+        
+        Toast.makeText(UrlEvaluatorActivity.this, inputUri.toString(), Toast.LENGTH_LONG);
+        
     }
 }
